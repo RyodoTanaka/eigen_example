93b09929c7db60fd1304853809c7f9e776fd19c3
RyodoTanaka <groadpg@gmail.com>
2020-02-25 18:36:19 +0900

    Update to add psuedo inverse matrix example

---
+++ src/psuedo_matrix_qr.cpp
@@ -0,0 +1,23 @@
+#include <Eigen/Core>
+#include <Eigen/QR>
+#include <iostream>
+using namespace Eigen;
+
+int main()
+{
+  MatrixXd A = MatrixXd::Random(5, 8);
+  VectorXd b = VectorXd::Random(5);
+  VectorXd b0 = b; //save b because we are going to write over during computations  // You should have already done something like this.
+  ColPivHouseholderQR<MatrixXd> qr(A.transpose());
+  MatrixXd Q = qr.matrixQ();  // This computes A^+ * b
+  auto R = qr.matrixQR().topRows(5).transpose().template triangularView<Lower>();
+  b = qr.colsPermutation().transpose()* b;
+  R.solveInPlace(b);
+  VectorXd x = Q.leftCols(5) * b;  // just checking the result
+  std::cout << "x = " << x.transpose() << std::endl;
+  std::cout << "||A*x-b0|| = " << (A * x - b0).norm() << std::endl;  // for comparison, the solve of CompleteOrthogonalDecomposition does a pseudo-inverse
+  MatrixXd pinvA = A.completeOrthogonalDecomposition().pseudoInverse();
+  VectorXd x0 = pinvA * b0;  std::cout << "x0 = " << x0.transpose() << std::endl;
+  std::cout << "||A*x0-b0|| = " << (A * x0 - b0).norm() << std::endl;
+  std::cout << "||x - x0|| = " << (x0 - x).norm() << std::endl;
+}
